a38856dc4d3893041acd80120d5f3f5db4b07f61
kitsunai <kitsunai@sanritz.co.jp>
2013-10-25 20:55:47 +0900

    add files

---
+++ Commons/DesignPattern/Observer.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace Commons.DesignPattern {
+	/// <summary>
+	/// 拡張メソッド集
+	/// </summary>
+	static class ObjectExtenslons {
+		/// <summary>
+		/// オブジェクトのプロパティ値を名前から取得
+		/// </summary>
+		/// <param name="item">オブジェクト</param>
+		/// <param name="propertyName">プロパティ名</param>
+		/// <returns>オブジェクトのプロパティの値</returns>
+		public static object Eval(this object item, string propertyName) {
+			var propertyInfo = item.GetType().GetProperty(propertyName);
+			return propertyInfo == null ? null : propertyInfo.GetValue(item, null);
+		}
+
+		/// <summary>
+		/// Expressionからメンバー名を取得
+		/// </summary>
+		/// <typeparam name="ObjectType">このメソッドを呼び出したオブジェクトの型</typeparam>
+		/// <typeparam name="MemberType">オブジェクトのメンバーの型</typeparam>
+		/// <param name="this">呼び出し元のオブジェクト</param>
+		/// <param name="expression">Expression</param>
+		/// <returns>オブジェクトのメンバの名前</returns>
+		public static string GetMemberName<ObjectType, MemberType>(this ObjectType @this, Expression<Func<ObjectType, MemberType>> expression) {
+			return ((MemberExpression)expression.Body).Member.Name;
+		}
+	}
+
+	/// <summary>
+	/// 更新を監視される側
+	/// </summary>
+	public abstract class Observable {
+		/// <summary>
+		/// バインドされたObserverの更新関数を呼び出す
+		/// </summary>
+		public event Action<string> Updata;
+
+		/// <summary>
+		/// イベントの呼び出し関数
+		/// </summary>
+		/// <param name="propertyName">このメソッドを呼び出したプロパティの値</param>
+		protected void RaiseUpdate([CallerMemberName] string propertyName = "") {
+			if(Updata != null) {
+				Updata(propertyName);
+			}
+		}
+	}
+
+	/// <summary>
+	/// 更新を監視する側
+	/// </summary>
+	public abstract class Observer<ObservableType> where ObservableType : Observable {
+		/// <summary>
+		/// 各種アップデートコマンドを保管する
+		/// </summary>
+		Dictionary<string, Action<object>> updateExpressions = new Dictionary<string, Action<object>>();
+
+		/// <summary>
+		/// バインドしたObservable
+		/// </summary>
+		ObservableType dataSource = null;
+
+		/// <summary>
+		/// Observableをバインドする
+		/// </summary>
+		public ObservableType DataSource {
+			set {
+				if(dataSource != null)
+					dataSource.Updata -= Update;
+				dataSource = value;
+				value.Updata += Update;
+			}
+			get { return dataSource; }
+		}
+
+		/// <summary>
+		/// アップデートメソッドをExpressionをもとに登録する
+		/// </summary>
+		/// <typeparam name="PropertyType">Observableのプロパティ</typeparam>
+		/// <param name="propertyExpression">対応するプロパティ</param>
+		/// <param name="updateAction">更新時に呼び出すメソッド</param>
+		protected void AddUpdateAction<PropertyType>(Expression<Func<ObservableType, PropertyType>> propertyExpression, Action<object> updateAction) {
+			_AddUpdateAction(dataSource.GetMemberName(propertyExpression), updateAction);
+		}
+
+		/// <summary>
+		/// アップデートメソッドをExpressionをもとに登録する(横着版)
+		/// </summary>
+		/// <typeparam name="PropertyType">Observableのプロパティ</typeparam>
+		/// <param name="propertyExpression">対応するプロパティ</param>
+		/// <param name="updateAction">更新時に呼び出すメソッド</param>
+		public void AddUpdateAction<PropertyType>(Expression<Func<ObservableType, PropertyType>> propertyExpression, Action<PropertyType> updateAction) {
+			Action<object> rapAction = obj => {
+				var prop = (PropertyType)obj;
+				updateAction(prop);
+			};
+			_AddUpdateAction(dataSource.GetMemberName(propertyExpression), rapAction);
+		}
+
+		/// <summary>
+		/// アップデートメソッドを文字列をもとに登録する
+		/// </summary>
+		/// <param name="propertyName">対応するプロパティ</param>
+		/// <param name="updateAction">更新時に呼び出すメソッド</param>
+		void _AddUpdateAction(string propertyName, Action<object> updateAction) {
+			updateExpressions[propertyName] = updateAction;
+		}
+
+		/// <summary>
+		/// 更新
+		/// </summary>
+		/// <param name="propertyName">更新されたプロパティ</param>
+		void Update(string propertyName) {
+			Action<object> updateAction;
+			if(updateExpressions.TryGetValue(propertyName, out updateAction))
+				updateAction(dataSource.Eval(propertyName));
+		}
+	}
+}
---
+++ Commons/Form/AppSettings.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+//下記のコードのprojectnameをリネームする
+//namespace projectname.Properties{
+//	[System.Configuration.SettingsProvider(typeof(My_settings.ConfigFileSettingsProvider))]
+//	internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+//	}
+//}
+
+namespace Commons.Form {
+
+	/// <summary>
+	/// Protraのconfディレクトリに設定を保存するための設定プロバイダー
+	/// </summary>
+	public class ConfigFileSettingsProvider : SettingsProvider {
+		private readonly string configFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), Application.ProductName + ".config");
+
+		/// <summary>
+		/// プロバイダーを初期化する。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="config"></param>
+		public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config) {
+			base.Initialize("ConfigFileSettingsProvider", config);
+		}
+
+		/// <summary>
+		/// 現在実行中のアプリケーションの名前を取得または設定する。
+		/// </summary>
+		public override string ApplicationName {
+			get { return Application.ProductName; }
+			set { }
+		}
+
+		/// <summary>
+		/// シリアライズ可能なKeyValuePair
+		/// </summary>
+		/// <typeparam name="K">キーの型</typeparam>
+		/// <typeparam name="V">値の型</typeparam>
+		[Serializable]
+		public struct KeyValuePair<K, V> {
+			/// <summary>
+			/// コンストラクタ
+			/// </summary>
+			/// <param name="key">キー</param>
+			/// <param name="value">値</param>
+			public KeyValuePair(K key, V value)
+				: this() {
+				Key = key;
+				Value = value;
+			}
+			/// <summary>
+			/// キーを取得または設定する。
+			/// </summary>
+			public K Key { get; set; }
+			/// <summary>
+			/// 値を取得または設定する。
+			/// </summary>
+			public V Value { get; set; }
+		}
+
+		/// <summary>
+		/// 設定ファイルを読んで指定された設定プロパティグループの値のコレクションを返す。
+		/// </summary>
+		/// <param name="context">現在のアプリケーションの使い方を記述しているSettingsContext。</param>
+		/// <param name="collection">値の取得対象となる設定プロパティグループを格納しているSettingsPropertyCollection。</param>
+		/// <returns>設定プロパティグループの値を格納しているSettingsPropertyValueCollection。</returns>
+		public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection collection) {
+			var dictionary = new Dictionary<string, Object>();
+			var valueCollection = new SettingsPropertyValueCollection();
+			var selializer = new XmlSerializer(typeof(List<KeyValuePair<string, object>>));
+			//デシリアライズ
+			try {
+				using(var reader = File.OpenText(configFile))
+					((List<KeyValuePair<string, Object>>)selializer.Deserialize(reader))
+						.ForEach(pair => {
+							dictionary[pair.Key] = pair.Value;
+						});
+			} catch(Exception e) {
+				if(!(e is IOException || e is InvalidOperationException))
+					throw;
+			}
+			//コレクションの代入う
+			foreach(SettingsProperty property in collection)
+				valueCollection.Add(new SettingsPropertyValue(property) {
+					SerializedValue = dictionary.ContainsKey(property.Name)
+										  ? dictionary[property.Name]
+										  : property.DefaultValue,
+					IsDirty = false
+				});
+			return valueCollection;
+		}
+
+		/// <summary>
+		/// 指定された設定プロパティグループの値を設定ファイルに保存します。
+		/// </summary>
+		/// <param name="context">現在のアプリケーションの使い方を記述しているSettingsContext。</param>
+		/// <param name="collection">保存する設定プロパティグループを格納しているSettingsPropertyCollection。</param>
+		public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection collection) {
+			var kvList = new List<KeyValuePair<string, Object>>();
+			var selializer = new XmlSerializer(kvList.GetType());
+			foreach(SettingsPropertyValue value in collection)
+				kvList.Add(new KeyValuePair<string, object>(value.Name, value.SerializedValue));
+			using(var stream = File.CreateText(configFile))
+				selializer.Serialize(stream, kvList);
+		}
+	}
+}
